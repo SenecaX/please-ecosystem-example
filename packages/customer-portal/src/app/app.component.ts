1cfaa2db0c95220de48a26ea71b00686042a8108
Kedaren <kedaren.dorasami@yocto.re>
2022-07-04 13:07:21 +0400

    msg

---
+++ packages/customer-portal/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'please-ecosystem-example-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'customer-portal';
+}
